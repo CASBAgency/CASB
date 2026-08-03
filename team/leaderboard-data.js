cc5211795fb8444c6ff566ec749eff81e062e5e7
Oikos Advisory <operation@casb2u.com>
2026-08-03 12:56:04 +0800

    Correct leaderboard from live Daily Report

--- team/leaderboard-data.js
+++ team/leaderboard-data.js
@@ -1,31 +1,31 @@
 window.CASB_LEADERBOARD = {
-  period: "June 2026",
-  asAt: "21 June 2026",
-  source: "CASB Daily Report",
+  period: "July 2026",
+  asAt: "30 July 2026",
+  source: "CASB Daily Report - GoalFullYear",
   rankings: {
     afyp: [
-      { name: "Lai Siew Ching", value: 76770.75, cases: 21, photo: "LaiSiewChing.png" },
-      { name: "Rock Ng Vin Kee", value: 26307.42, cases: 6, photo: "RockNg.png" },
-      { name: "Peri Tan Pei Li", value: 21274.02, cases: 7, photo: "Peri.png" },
-      { name: "Esther Tan Pui Yen", value: 21089.42, cases: 8, photo: "EstherTan.png" },
-      { name: "Lee Yun Teng", value: 10219.08, cases: 3 },
-      { name: "Wong Yew Peng", value: 9288.24, cases: 2, photo: "WongYewPeng.png" },
-      { name: "Gladys Heng Hui Shan", value: 6606.61, cases: 2, photo: "GladysHeng.png" },
-      { name: "Law Xin Hui", value: 5671, cases: 2 },
-      { name: "Tang Hui Shian", value: 5280, cases: 2, photo: "TangHuiShian.png" },
-      { name: "Allan Wong Tieng Hock", value: 5137.13, cases: -2, photo: "AllanWong.png" }
+      { name: "Lai Siew Ching", value: 92873.82, cases: 22, photo: "LaiSiewChing.png" },
+      { name: "Kong Siaw Ling", value: 40395.10, cases: 11, photo: "KongSiawLing.png" },
+      { name: "Peri Tan Pei Li", value: 32821.58, cases: 8, photo: "Peri.png" },
+      { name: "Wong Yew Peng", value: 30180.24, cases: 7, photo: "WongYewPeng.png" },
+      { name: "Law Xin Hui", value: 27465, cases: 8 },
+      { name: "Gladys Heng Hui Shan", value: 21545.03, cases: 8, photo: "GladysHeng.png" },
+      { name: "Rock Ng Vin Kee", value: 17950.65, cases: 6, photo: "RockNg.png" },
+      { name: "Jess Lau Siew Chin", value: 17288.70, cases: 3, photo: "JessLau.png" },
+      { name: "Esther Tan Pui Yen", value: 15172.19, cases: 6, photo: "EstherTan.png" },
+      { name: "Lorraine Kong Siew Feng", value: 13599.14, cases: 4, photo: "LorraineKong.png" }
     ],
     cases: [
-      { name: "Lai Siew Ching", value: 21, afyp: 76770.75, photo: "LaiSiewChing.png" },
-      { name: "Esther Tan Pui Yen", value: 8, afyp: 21089.42, photo: "EstherTan.png" },
-      { name: "Peri Tan Pei Li", value: 7, afyp: 21274.02, photo: "Peri.png" },
-      { name: "Rock Ng Vin Kee", value: 6, afyp: 26307.42, photo: "RockNg.png" },
-      { name: "Lee Yun Teng", value: 3, afyp: 10219.08 },
-      { name: "Wong Yew Peng", value: 2, afyp: 9288.24, photo: "WongYewPeng.png" },
-      { name: "Gladys Heng Hui Shan", value: 2, afyp: 6606.61, photo: "GladysHeng.png" },
-      { name: "Law Xin Hui", value: 2, afyp: 5671 },
-      { name: "Tang Hui Shian", value: 2, afyp: 5280, photo: "TangHuiShian.png" },
-      { name: "Tan Lay Ching", value: 1, afyp: 4711.96 }
+      { name: "Lai Siew Ching", value: 22, afyp: 92873.82, photo: "LaiSiewChing.png" },
+      { name: "Kong Siaw Ling", value: 11, afyp: 40395.10, photo: "KongSiawLing.png" },
+      { name: "Peri Tan Pei Li", value: 8, afyp: 32821.58, photo: "Peri.png" },
+      { name: "Law Xin Hui", value: 8, afyp: 27465 },
+      { name: "Gladys Heng Hui Shan", value: 8, afyp: 21545.03, photo: "GladysHeng.png" },
+      { name: "Wong Yew Peng", value: 7, afyp: 30180.24, photo: "WongYewPeng.png" },
+      { name: "Rock Ng Vin Kee", value: 6, afyp: 17950.65, photo: "RockNg.png" },
+      { name: "Esther Tan Pui Yen", value: 6, afyp: 15172.19, photo: "EstherTan.png" },
+      { name: "Lorraine Kong Siew Feng", value: 4, afyp: 13599.14, photo: "LorraineKong.png" },
+      { name: "Jackie Ong Chow Leng", value: 4, afyp: 11841.20, photo: "JackieOng.png" }
     ]
   }
 };
