9813b5ed718fdb2b772504ceba2a60058314bcf6
Oikos Advisory <operation@casb2u.com>
2026-08-03 12:43:01 +0800

    Publish CASB production leaderboard

---
+++ team/leaderboard-data.js
@@ -0,0 +1,31 @@
+window.CASB_LEADERBOARD = {
+  period: "June 2026",
+  asAt: "21 June 2026",
+  source: "CASB Daily Report",
+  rankings: {
+    afyp: [
+      { name: "Lai Siew Ching", value: 76770.75, cases: 21, photo: "LaiSiewChing.png" },
+      { name: "Rock Ng Vin Kee", value: 26307.42, cases: 6, photo: "RockNg.png" },
+      { name: "Peri Tan Pei Li", value: 21274.02, cases: 7, photo: "Peri.png" },
+      { name: "Esther Tan Pui Yen", value: 21089.42, cases: 8, photo: "EstherTan.png" },
+      { name: "Lee Yun Teng", value: 10219.08, cases: 3 },
+      { name: "Wong Yew Peng", value: 9288.24, cases: 2, photo: "WongYewPeng.png" },
+      { name: "Gladys Heng Hui Shan", value: 6606.61, cases: 2, photo: "GladysHeng.png" },
+      { name: "Law Xin Hui", value: 5671, cases: 2 },
+      { name: "Tang Hui Shian", value: 5280, cases: 2, photo: "TangHuiShian.png" },
+      { name: "Allan Wong Tieng Hock", value: 5137.13, cases: -2, photo: "AllanWong.png" }
+    ],
+    cases: [
+      { name: "Lai Siew Ching", value: 21, afyp: 76770.75, photo: "LaiSiewChing.png" },
+      { name: "Esther Tan Pui Yen", value: 8, afyp: 21089.42, photo: "EstherTan.png" },
+      { name: "Peri Tan Pei Li", value: 7, afyp: 21274.02, photo: "Peri.png" },
+      { name: "Rock Ng Vin Kee", value: 6, afyp: 26307.42, photo: "RockNg.png" },
+      { name: "Lee Yun Teng", value: 3, afyp: 10219.08 },
+      { name: "Wong Yew Peng", value: 2, afyp: 9288.24, photo: "WongYewPeng.png" },
+      { name: "Gladys Heng Hui Shan", value: 2, afyp: 6606.61, photo: "GladysHeng.png" },
+      { name: "Law Xin Hui", value: 2, afyp: 5671 },
+      { name: "Tang Hui Shian", value: 2, afyp: 5280, photo: "TangHuiShian.png" },
+      { name: "Tan Lay Ching", value: 1, afyp: 4711.96 }
+    ]
+  }
+};
